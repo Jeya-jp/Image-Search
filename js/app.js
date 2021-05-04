45f630c6951c76b80b6eb81124172ea15b2583c3
sachin <sachinabs.js@gmail.com>
2021-05-04 20:25:00 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -4,22 +4,31 @@ function perform()
     let checkBox2 = document.getElementById("check2").checked;
     
     let value = document.getElementById("key").value;
-    let website = ["https://www.pexels.com/search/","https://unsplash.com/s/photos/"];
-    
-   
+    if(value =="")
+    {
+        alert("Enter Keyword..!")
+    }
+    else
+    {
+        if(checkBox1 == false & checkBox2 == false)
+        {
+            alert("Choose website");
+        }
+        else
+        {
+        let website = ["https://www.pexels.com/search/","https://unsplash.com/s/photos/"];
         if(checkBox1 == true)
         {
             chrome.tabs.create({url: website[0] + value, selected: true});
-            // window.open(website[0] + value);
-            // alert("0")
         }
         if(checkBox2 == true)
         {
-            // alert("1")
-            // window.open(website[1] + value);
             chrome.tabs.create({url: website[1] + value, selected: true});
         }
+        }
 
+    }
+    
         
     
 }
@@ -28,3 +37,5 @@ document.addEventListener("DOMContentLoaded",function()
 {
     document.getElementById("action").addEventListener("click", perform)
 });
+
+
